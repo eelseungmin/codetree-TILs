3ba8903b377696500dc141de49b47c28f75880ce
eelseungmin <berrypicker777@gmail.com>
2023-12-18 20:54:34 +0900

    Title: 거울에 레이저 쏘기 2, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231218/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class Main {
+    /**
+    1~4N까지의 방향을 정한다.
+    각 모양에 해당 방향에서의 레이저가 부딪힐 때 어느 방향으로 휘는지 정한다.
+    휠 때 무조건 휘는 방향으로 한 칸 이동한다.
+    아래쪽
+    /: 오른쪽, \: 왼쪽
+    왼쪽
+    /: 아래쪽, \: 위쪽
+    위쪽
+    /: 왼쪽, \: 오른쪽
+    오른쪽
+    /: 위쪽, \: 아래쪽
+    */
+    static int[] dx = {1, 0, -1, 0}; // 레이저가 오는 방향 순서대로
+    static int[] dy = {0, -1, 0, 1};
+    static int x = 0;
+    static int y = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[][] map = new char[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            String tmp = sc.next();
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = tmp.charAt(j - 1);
+            }
+        }
+        int k = sc.nextInt();
+        int dir = (k - 1) / n;
+        initialize(n, k);
+        int ans = 0;
+        while (true) {
+            dir = changeDir(dir, map[x][y]);
+            ans++;
+
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx <= 0 || nx > n || ny <= 0 || ny > n) {
+                break;
+            }
+            x = nx;
+            y = ny;
+        }
+
+        System.out.print(ans);
+    }
+
+    public static void initialize(int n, int k) {
+        if (k <= n) {
+            x = 1; y = k;
+        } else if (k <= 2 * n) {
+            x = k - n; y = n;
+        } else if (k <= 3 * n) {
+            x = n; y = n - (k - 2 * n) + 1;
+        } else {
+            x = n - (k - 3 * n) + 1; y = 1;
+        }
+    }
+
+    public static int changeDir(int dir, char ch) {
+        int res = -1;
+        if (dir == 0) {
+            if (ch == '/') {
+                res = 3;
+            } else {
+                res = 1;
+            }
+        } else if (dir == 1) {
+            if (ch == '/') {
+                res = 0;
+            } else {
+                res = 2;
+            }
+        } else if (dir == 2) {
+            if (ch == '/') {
+                res = 1;
+            } else {
+                res = 3;
+            }
+        } else {
+            if (ch == '/') {
+                res = 2;
+            } else {
+                res = 0;
+            }
+        }
+
+        return res;
+    }
+}
