fc52d452c10354e876925f4acf44a7701701cc28
eelseungmin <berrypicker777@gmail.com>
2023-12-18 21:37:58 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231218/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 231218/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -16,19 +16,19 @@ public class Main {
         map = new int[n][m];
         int num = 0;
         int dir = 0;
-        while (num < n * m) {
+        while (true) {
             num++;
             map[x][y] = num;
+            if (num >= n * m) {
+                break;
+            }
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            if (0 <= nx && nx < n && 0 <= ny && ny < n && map[nx][ny] == 0) {
-                x = nx;
-                y = ny;
-            } else {
+            if ((nx < 0 || nx >= n || ny < 0 || ny >= n) || map[nx][ny] != 0) {
                 dir = (dir + 1) % 4;
-                x += dx[dir];
-                y += dy[dir];
             }
+            x += dx[dir];
+            y += dy[dir];
         }
 
         StringBuilder sb = new StringBuilder();
