4e7123e0091351f5e35de0c957fbc0fdbb2dc715
eelseungmin <berrypicker777@gmail.com>
2023-12-18 22:24:48 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231218/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 231218/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -4,7 +4,7 @@ public class Main {
     static int n = 0;
     static int m = 0;
     static int[][] map;
-    static int[] dx = {1, 0, -1, 0};
+    static int[] dx = {1, 0, -1, 0}; // 남 동 북 서
     static int[] dy = {0, 1, 0, -1};
     static int x = 0;
     static int y = 0;
@@ -13,8 +13,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        map = new int[m][n];
-        int num = 2;
+        map = new int[n][m];
+        int num = 1;
         int dir = 0;
         map[0][0] = 1;
         while (true) {
@@ -23,12 +23,12 @@ public class Main {
             }
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            if ((nx < 0 || nx >= n || ny < 0 || ny >= n) || map[nx][ny] != 0) {
+            if ((nx < 0 || nx >= n || ny < 0 || ny >= m) || map[nx][ny] != 0) {
                 dir = (dir + 1) % 4;
             }
             x += dx[dir];
             y += dy[dir];
-            map[x][y] = num++;
+            map[x][y] = ++num;
         }
 
         StringBuilder sb = new StringBuilder();
