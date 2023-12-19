2953a1df387ded3d0aa4ca62dbb667a2e507babc
eelseungmin <berrypicker777@gmail.com>
2023-12-19 19:07:18 +0900

    Title: 이상한 진수 2, Time: 208ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231219/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String s = String.valueOf(a);
+        int n = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '0') {
+                ch = '1';
+            } else {
+                ch = '0';
+            }
+            String tmp = "";
+            if (i == 0) {
+                tmp = ch + s.substring(i + 1, s.length());
+            } else if (0 <= i && i < s.length() - 1) {
+                tmp = s.substring(0, i) + ch + s.substring(i + 1, s.length());
+            } else {
+                tmp = s.substring(0, i) + ch;
+            }
+            n = Math.max(n, Integer.parseInt(tmp, 2));
+        }
+
+        System.out.print(n);
+    }
+}
