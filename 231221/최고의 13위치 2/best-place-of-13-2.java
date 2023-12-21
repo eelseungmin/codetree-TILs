42bfb9244d31083a467e993585f3164b2b4483be
eelseungmin <berrypicker777@gmail.com>
2023-12-21 19:32:02 +0900

    Title: 최고의 13위치 2, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231221/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                for (int k = i + 1; k < n; k++) {
+                    for (int h = 0; h < n - 2; h++) {
+                        ans = Math.max(ans, map[i][j] + map[i][j + 1] + map[i][j + 2] + map[k][h] + map[k][h + 1] + map[k][h + 2]);
+                    }
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
