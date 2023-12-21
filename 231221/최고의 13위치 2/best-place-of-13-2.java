c6fcff78c4d81020d188806c5d21ea2cb9615b40
eelseungmin <berrypicker777@gmail.com>
2023-12-21 21:08:39 +0900

    Title: 최고의 13위치 2, Time: 221ms, Memory: 10MB, Status: Passed - CodeTree

--- 231221/최고의 13위치 2/best-place-of-13-2.java
+++ 231221/최고의 13위치 2/best-place-of-13-2.java
@@ -15,8 +15,9 @@ public class Main {
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 2; j++) {
-                for (int k = i + 1; k < n; k++) {
+                for (int k = 0; k < n; k++) {
                     for (int h = 0; h < n - 2; h++) {
+                        if (i == k && Math.abs(j - h) <= 2) continue;
                         ans = Math.max(ans, map[i][j] + map[i][j + 1] + map[i][j + 2] + map[k][h] + map[k][h + 1] + map[k][h + 2]);
                     }
                 }
