3d0c58fb31a82535c033f3f61a05ed9d830262ed
eelseungmin <berrypicker777@gmail.com>
2023-12-30 00:00:17 +0900

    Title: 바구니 안의 사탕 2, Time: 172ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231230/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] basket = new int[401];
+        for (int i = 0; i < n; i++) {
+            int candy = sc.nextInt();
+            int bas = sc.nextInt();
+            basket[bas] += candy;
+        }
+
+        int ans = 0;
+        for (int i = 0; i <= 400 - 2 * k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + 2 * k + 1; j++) {
+                sum += basket[j];
+            }
+            ans = Math.max(sum, ans);
+        }
+
+        System.out.print(ans);
+    }
+}
