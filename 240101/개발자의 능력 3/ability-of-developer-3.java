35c6780f115196325aaf708952a90ff6f4002b94
eelseungmin <berrypicker777@gmail.com>
2024-01-01 18:00:50 +0900

    Title: 개발자의 능력 3, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240101/개발자의 능력 3/ability-of-developer-3.java
+++ 240101/개발자의 능력 3/ability-of-developer-3.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
 public class Main {
+    public static int[] dev;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int[] dev = new int[6];
+        dev = new int[6];
         for (int i = 0; i < 6; i++) {
             dev[i] = sc.nextInt();
         }
@@ -13,7 +15,7 @@ public class Main {
         for (int i = 0; i < 6; i++) {
             for (int j = i + 1; j < 6; j++) {
                 for (int k = j + 1; k < 6; k++) {
-                    ans = Math.min(ans, calcDiff(dev, i, j, k));
+                    ans = Math.min(ans, calcDiff(i, j, k));
                 }
             }
         }
@@ -21,15 +23,14 @@ public class Main {
         System.out.print(ans);
     }
 
-    public static int calcDiff(int[] arr, int i, int j, int k) {
-        int sum1 = arr[i] + arr[j] + arr[k];
+    public static int calcDiff(int i, int j, int k) {
+        int sum1 = dev[i] + dev[j] + dev[k];
         int sum2 = 0;
-        for (int a = 0; a < 6; i++) {
-            sum2 += arr[a];
+        for (int a = 0; a < 6; a++) {
+            sum2 += dev[a];
         }
         sum2 -= sum1;
 
-        System.out.println(i + " " + j + " " + k);
         return Math.abs(sum1 - sum2);
     }
 }
