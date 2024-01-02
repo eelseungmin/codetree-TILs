f39c6b500b982630154e6709078900922fa7ebea
eelseungmin <berrypicker777@gmail.com>
2024-01-02 19:51:51 +0900

    Title: 이상한 폭탄 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240102/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] bomb = new int[n];
+        for (int i = 0; i < n; i++) {
+            bomb[i] = sc.nextInt();
+        }
+
+        int max = -1;
+        for (int i = 0; i < n - k; i++) {
+            for (int j = i; j <= i + k; j++) {
+                if (i != j && bomb[i] == bomb[j]) {
+                    max = Math.max(max, bomb[j]);
+                }
+            }
+        }
+
+        System.out.print(max);
+    }
+}
