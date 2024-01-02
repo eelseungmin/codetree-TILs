fc29e2c06d2aa8b25bd4a8928984699c7294b3ea
eelseungmin <berrypicker777@gmail.com>
2024-01-02 17:15:01 +0900

    Title: 삼각형 만들기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240102/삼각형 만들기/create-triangle.java
+++ 240102/삼각형 만들기/create-triangle.java
@@ -18,11 +18,11 @@ public class Main {
                     if (i == j || j == k || k == i) continue;
                     if (seg[i][0] != seg[j][0] && seg[j][0] != seg[k][0] && seg[k][0] != seg[i][0]) continue;
                     if (seg[i][1] != seg[j][1] && seg[j][1] != seg[k][1] && seg[k][1] != seg[i][1]) continue;
-                    max = Math.max(max, (double) Math.abs((seg[i][0] * seg[j][1] + seg[j][0] * seg[k][1] + seg[k][0] * seg[i][1]) - (seg[j][0] * seg[i][1] + seg[k][0] * seg[j][1] + seg[i][0] * seg[k][1])) / 2.0);
+                    max = Math.max(max, (double) Math.abs((seg[i][0] * seg[j][1] + seg[j][0] * seg[k][1] + seg[k][0] * seg[i][1]) - (seg[j][0] * seg[i][1] + seg[k][0] * seg[j][1] + seg[i][0] * seg[k][1])) * 0.5);
                 }
             }
         }
 
-        System.out.print(max * 2);
+        System.out.print((int) max * 2);
     }
 }
