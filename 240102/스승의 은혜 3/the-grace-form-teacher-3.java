ba8cef4da4ad06c3ae4531956893cd496f66d4b9
eelseungmin <berrypicker777@gmail.com>
2024-01-02 20:23:46 +0900

    Title: 스승의 은혜 3, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240102/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240102/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-public class Main {
-    static class Student implements Comparable<Student> {
+class Student implements Comparable<Student> {
         int price;
         int trans;
 
@@ -19,6 +18,7 @@ public class Main {
         }
     }
 
+public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
