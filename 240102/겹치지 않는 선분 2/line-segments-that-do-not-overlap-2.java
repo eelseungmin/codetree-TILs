39ee1d00bd55c48752eb70462668ad71e3a047b4
eelseungmin <berrypicker777@gmail.com>
2024-01-02 21:59:44 +0900

    Title: 겹치지 않는 선분 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240102/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            boolean flag = false;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if ((x1[i] >= x1[j] && x2[i] <= x2[j]) || (x1[j] >= x1[i] && x2[j] <= x1[i])) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag) ans++;
+        }
+
+        System.out.print(ans);
+    }
+}
