19232599db66891ae9b7c2902c04f26205b2c477
eelseungmin <berrypicker777@gmail.com>
2024-01-03 19:24:02 +0900

    Title: 정보에 따른 숫자 2, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/정보에 따른 숫자 2/number-based-on-information-2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char[] pos = new char[1001];
+        for (int i = 0; i < T; i++) {
+            char ch = sc.next().charAt(0);
+            pos[sc.nextInt()] = ch;
+        }
+
+        int ans = 0;
+        for (int i = a; i <= b; i++) {
+            int d1 = Integer.MAX_VALUE;
+            int d2 = Integer.MAX_VALUE;
+            for (int j = a; j <= b; j++) {
+                if (pos[j] == 'S') {
+                    d1 = Math.min(d1, Math.abs(j - i));
+                } else if (pos[j] == 'N') {
+                    d2 = Math.min(d2, Math.abs(j - i));
+                }
+            }
+            if (d1 <= d2) {
+                ans++;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
