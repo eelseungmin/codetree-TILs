1c640f92dff539d187bf1e6355c277360ad7bdac
eelseungmin <berrypicker777@gmail.com>
2024-01-04 10:41:30 +0900

    Title: 스승의 은혜 3, Time: 358ms, Memory: 16MB, Status: Passed - CodeTree

--- 240104/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 240104/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -1,53 +1,37 @@
 import java.util.*;
 
-class Student implements Comparable<Student> {
-        int price;
-        int trans;
-
-        Student(int price, int trans) {
-            this.price = price;
-            this.trans = trans;
-        }
-
-        @Override
-        public int compareTo(Student s) {
-            if (this.price + this.trans == s.price + s.trans)
-                return (this.price / 2 + this.trans) - (s.price / 2 + s.trans);
-            return (this.price + this.trans) - (s.price + s.trans);
-        }
-    }
-
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int b = sc.nextInt();
-        Student[] s = new Student[n];
+        int[] p = new int[n];
+        int[] s = new int[n];
         for (int i = 0; i < n; i++) {
-            s[i] = new Student(sc.nextInt(), sc.nextInt());
+            p[i] = sc.nextInt();
+            s[i] = sc.nextInt();
         }
-        Arrays.sort(s);
 
         int ans = 0;
+        // 할인할 선물 선택
         for (int i = 0; i < n; i++) {
-            int budget = b;
+            int[] tmp = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == j) tmp[j] = p[j] / 2 + s[j];
+                else tmp[j] = p[j] + s[j];
+            }
+            Arrays.sort(tmp);
+
+            int budget = 0;
             int cnt = 0;
             for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    budget -= s[j].price / 2;
-                    budget -= s[j].trans;
-                } else {
-                    budget = budget - s[j].price - s[j].trans;
-                }
-                 
-                if (budget >= 0) {
-                    cnt++;
-                }
-                else {
-                    break;
-                }
+                budget += tmp[j];
+
+                if (budget <= b) cnt++;
+                else break;
             }
+
             ans = Math.max(ans, cnt);
         }
 
