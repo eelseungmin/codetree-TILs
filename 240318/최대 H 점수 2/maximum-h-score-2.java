4c7f4ab8729dfb2bcf327bd815dbd55ca1548036
eelseungmin <berrypicker777@gmail.com>
2024-03-18 18:58:35 +0900

    Title: 최대 H 점수 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240318/최대 H 점수 2/maximum-h-score-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int l = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        for (int h = 0; h <= 100; h++) {
+            
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] >= h) {
+                    cnt++;
+                }
+            }
+            if (cnt >= h) {
+                max = Math.max(max, h);
+            }
+        }
+
+        System.out.print(max);
+    }
+}
