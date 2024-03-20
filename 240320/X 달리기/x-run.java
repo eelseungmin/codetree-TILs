95d7548788ebbe2290d4c5d04000f14eeb11fabc
eelseungmin <berrypicker777@gmail.com>
2024-03-20 17:36:10 +0900

    Title: X 달리기, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240320/X 달리기/x-run.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < x; i++) {
+            int speed = 1;
+            int time = 0;
+            int road = 0;
+            while (road < x) {
+                road += speed;
+
+                if (road < i) {
+                    speed++;
+                } else if (speed > 1) {
+                    speed--;
+                }
+
+                time++;
+            } 
+            if (speed == 1) {
+                ans = Math.min(ans, time);
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
