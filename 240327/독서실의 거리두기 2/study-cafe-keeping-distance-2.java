21cb0d23446759522b847e4d9596c02a168ec13e
eelseungmin <berrypicker777@gmail.com>
2024-03-27 14:48:25 +0900

    Title: 독서실의 거리두기 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240327/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
+++ 240327/독서실의 거리두기 2/study-cafe-keeping-distance-2.java
@@ -29,7 +29,7 @@ public class Main {
         if (seat[n - 1] == '0') {
             for (int i = n - 2; i >= 0; i--) {
                 if (seat[i] == '1') {
-                    if (diff < n - 1 - i) {
+                    if ((l + r) / 2 - l < n - 1 - i) {
                         flag = true;
                     }
                     break;
