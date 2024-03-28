d1d8c52e366082e0b4d3564bc3f1b985daad49d4
eelseungmin <berrypicker777@gmail.com>
2024-03-28 13:10:16 +0900

    Title: ABC 줄 세우기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240328/ABC 줄 세우기/abc-line-up.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        char[] pos = new char[n];
+        for (int i = 0; i < n; i++) {
+            pos[i] = sc.next().charAt(0);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (pos[i] != (char)((int)'A' + i)) {
+                for (int j = 0; j < n; j++) {
+                    if (pos[j] == (char)((int)'A' + i)) {
+                        char temp = pos[i];
+                        pos[i] = pos[j];
+                        pos[j] = temp;
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
