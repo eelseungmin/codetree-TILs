a0058fd1c9d5e9c28880421cff90cf93fa35d72c
eelseungmin <berrypicker777@gmail.com>
2024-03-29 10:34:12 +0900

    Title: 순위 경쟁2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240329/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 0 ;
+        char[] c = new char[n];
+        int[] s = new int[n];
+        for (int i = 0; i < n; i++) {
+            c[i] = sc.next().charAt(0);
+            s[i] = sc.nextInt();
+        }
+
+        char win = '\u0000';
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (c[i] == 'A') {
+                a += s[i];
+            } else {
+                b += s[i];
+            }
+
+            if (a > b) {
+                if (win == '\u0000') {
+                    win = 'a';
+                    ans++;
+                } else if (win == 'b') {
+                    ans++;
+                } 
+            } else if (a < b) {
+                if (win == '\u0000') {
+                    win = 'b';
+                    ans++;
+                } else if (win == 'a') {
+                    ans++;
+                }
+            } else {
+                if (win != '\u0000') {
+                    win = '\u0000';
+                    ans++;
+                }
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
