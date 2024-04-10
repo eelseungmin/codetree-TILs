c7debf3ebee2df7dfaef24ac154074470ec801c6
eelseungmin <berrypicker777@gmail.com>
2024-04-10 17:17:36 +0900

    Title: 트로미노, Time: 412ms, Memory: 23MB, Status: Passed - CodeTree

--- 240410/트로미노/tromino.java
+++ 240410/트로미노/tromino.java
@@ -68,7 +68,7 @@ public class Main {
 
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 ans = Math.max(ans, getMaxSum(i, j));
             }
         }
