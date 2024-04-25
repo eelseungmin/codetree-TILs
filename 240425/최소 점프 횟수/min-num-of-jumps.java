7a223e805c724ece6bbdcf41ca3fe71792d16c3a
eelseungmin <berrypicker777@gmail.com>
2024-04-25 17:47:07 +0900

    Title: 최소 점프 횟수, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240425/최소 점프 횟수/min-num-of-jumps.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int ans = Integer.MAX_VALUE;
+    static boolean flag = true;
+    static int[] num;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        jump(0, 0);
+
+        if (flag) {
+            System.out.print(ans);
+        } else {
+            System.out.print(-1);
+        }
+    }
+
+    static void jump(int idx, int cnt) {
+        if (idx == n - 1) {
+            ans = Math.min(ans, cnt);
+            return;
+        }
+
+        if (num[idx] == 0) {
+            flag = false;
+            return;
+        }
+        for (int i = 1; i <= num[idx]; i++) {
+            jump(idx + i, cnt + 1);
+        }
+    }
+}
