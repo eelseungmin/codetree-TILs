dc0b17779cae42a6bc95eb9c33e517717cd11635
eelseungmin <berrypicker777@gmail.com>
2024-04-27 19:50:05 +0900

    Title: 1차원 윷놀이, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240427/1차원 윷놀이/yutnori-1d.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    static int n, m, k, ans;
+    static int[] possible;
+    static int[] dist;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+        possible = new int[n];
+        dist = new int[k];
+        for (int i = 0; i < n; i++) {
+            possible[i] = sc.nextInt();
+        }
+
+        go(0);
+
+        System.out.print(ans);
+    }
+
+    static void go(int idx) {
+        if (idx == n) {
+            int score = 0;
+            for (int i = 0; i < k; i++) {
+                if (dist[i] >= m) {
+                    score++;
+                }
+            }
+            ans = Math.max(ans, score);
+            return;
+        }
+
+        for (int i = 0; i < k; i++) {
+            dist[i] += possible[idx];
+            go(idx + 1);
+            dist[i] -= possible[idx];
+        }
+    }
+}
