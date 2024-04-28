253921f139c93b09253de9b87f4f3149eb4ae8b1
eelseungmin <berrypicker777@gmail.com>
2024-04-28 19:15:22 +0900

    Title: n개 중에 m개 뽑기, Time: 178ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240428/n개 중에 m개 뽑기/n-choose-m.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static boolean[] isUsed;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[m];
+        isUsed = new boolean[n + 1];
+
+        choose(0, 1);
+    }
+
+    static void choose(int idx, int start) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                arr[idx] = i;
+                choose(idx + 1, i + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
