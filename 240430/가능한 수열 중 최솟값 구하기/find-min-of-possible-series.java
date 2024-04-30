dd9321daf696a3b61d55ee714cd0bd0f62c9aac2
eelseungmin <berrypicker777@gmail.com>
2024-04-30 14:15:11 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240430/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+
+        choose(0);
+
+        System.out.print(list.get(0));
+    }
+
+    static void choose(int idx) {
+        if (idx == n) {
+            if (isPossible()) {
+                String ans = "";
+                for (int i = 0; i < n; i++) {
+                    ans += arr[i] + "";
+                }
+                list.add(ans);
+            }
+            return;
+        }
+
+        for (int i = 4; i <= 6; i++) {
+            arr[idx] = i;
+            choose(idx + 1);
+        }
+    }
+
+    static boolean isPossible() {
+        for (int i = 1; i <= n / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                String tmp1 = "";
+                String tmp2 = "";
+                for (int k = j; k < j + i; k++) {
+                    tmp1 += arr[k];
+                }
+                for (int k = j + i; k < j + 2 * i; k++) {
+                    tmp2 += arr[k];
+                }
+                
+                if (tmp1.equals(tmp2)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;  
+    }
+}
