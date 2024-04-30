0fa5f04f66ca744ae900195dc31fd82ea4a3dec9
eelseungmin <berrypicker777@gmail.com>
2024-04-30 15:35:04 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 602ms, Memory: 56MB, Status: Passed - CodeTree

--- 240430/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
+++ 240430/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.java
@@ -2,44 +2,43 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] arr;
+    static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        arr = new int[n];
 
         choose(0);
     }
 
     static void choose(int idx) {
         if (idx == n) {
-            if (isPossible()) {
-                for (int i = 0; i < n; i++) {
-                    System.out.print(arr[i]);
-                }
-                System.exit(0);
+            for (int i = 0; i < n; i++) {
+                System.out.print(list.get(i));
             }
-            return;
+            System.exit(0);
         }
 
         for (int i = 4; i <= 6; i++) {
-            arr[idx] = i;
-            choose(idx + 1);
+            list.add(i);
+            if (isPossible()) {
+                choose(idx + 1);
+            }
+            list.remove(list.size() - 1);
         }
     }
 
     static boolean isPossible() {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j + 2 * i <= n; j++) {
+        for (int i = 1; i <= list.size(); i++) {
+            for (int j = 0; j + 2 * i <= list.size(); j++) {
                 String tmp1 = "";
                 String tmp2 = "";
                 for (int k = j; k < j + i; k++) {
-                    tmp1 += arr[k];
+                    tmp1 += list.get(k);
                 }
                 for (int k = j + i; k < j + 2 * i; k++) {
-                    tmp2 += arr[k];
+                    tmp2 += list.get(k);
                 }
                 
                 if (tmp1.equals(tmp2)) {
