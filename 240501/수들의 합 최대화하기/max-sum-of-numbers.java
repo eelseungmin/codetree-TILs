8ade65ed12ca893f1552c3ce952ae3a86c5f7171
eelseungmin <berrypicker777@gmail.com>
2024-05-01 16:24:23 +0900

    Title: 수들의 합 최대화하기, Time: 211ms, Memory: 10MB, Status: Passed - CodeTree

--- 240501/수들의 합 최대화하기/max-sum-of-numbers.java
+++ 240501/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -3,72 +3,39 @@ import java.util.*;
 public class Main {
     static int n, ans;
     static int[][] num;
-    static boolean[][] vis;
+    static boolean[] vis;
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         num = new int[n][n];
-        vis = new boolean[n][n];
+        vis = new boolean[n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 num[i][j] = sc.nextInt();
             }
         }
 
-        choose(0);
+        choose(0, 0);
 
         System.out.print(ans);
     }
-
-    static void choose(int idx) {
-        if (idx == n) {
-            ans = Math.max(ans, calcSum());
+    
+    // 어차피 각 열마다, 각 행마다 한번씩은 칠해줘야 하므로
+    // n X n의 모든 칸에 대해 고려하는게 아니라 각 행마다 열 하나하나에 대해 백트래킹을 수행하는 식으로 구한다.
+    static void choose(int row, int sum) {
+        if (row == n) {
+            ans = Math.max(ans, sum);
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!vis[i][j]) {
-                    vis[i][j] = true;
-                    if (check())
-                        choose(idx + 1);
-                    vis[i][j] = false;
-                }
+            if (!vis[i]) {
+                vis[i] = true;
+                choose(row + 1, sum + num[row][i]);
+                vis[i] = false;
             }
         }
-    }
-
-    static boolean check() {
-        boolean[] row = new boolean[n];
-        boolean[] col = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (vis[i][j]) {
-                    if (!row[i]) row[i] = true;
-                    else return false;
-                    if (!col[j]) col[j] = true;
-                    else return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    static int calcSum() {
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (vis[i][j]) {
-                    sum += num[i][j];
-                }
-            }
-        }
-
-        return sum;
     }
 }
