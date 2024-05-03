6ea73a779fc3d85a09a25ca6de1e3ce6b825ed0e
eelseungmin <berrypicker777@gmail.com>
2024-05-03 22:21:50 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240503/피보나치 수/fibonacci-number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        System.out.print(dp[n]);
+    }
+}
