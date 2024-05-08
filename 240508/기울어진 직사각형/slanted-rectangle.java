fb0268b6d600ac2521b8714faf76275701b94055
eelseungmin <berrypicker777@gmail.com>
2024-05-08 14:09:10 +0900

    Title: 기울어진 직사각형, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240508/기울어진 직사각형/slanted-rectangle.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Main {
+    static int n, ans;
+    static int[] dx = {-1, -1, 1, 1}; // 1 - 2 - 3 - 4
+    static int[] dy = {1, -1, -1, 1};
+    static int[][] map;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        // 시작 위치를 기억해두고 숫자대로 순회를 하면서 합을 구한 뒤 최대값 갱신
+        // 이때 격자를 벗어나기 전까지 계속해서 한 방향으로 이동
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int x = i;
+                int y = j;
+
+                int sum = 0;
+                boolean flag = false;
+                // flag가 true일 때 최대값 갱신
+                for (int k = 0; k < 4; k++) {
+                    int cnt = 0;
+                    while (true) {
+                        x += dx[k];
+                        y += dy[k];
+                        if (!inRange(x, y)) {
+                            if (cnt < 1) break;
+                            else {
+                                x -= dx[k];
+                                y -= dy[k];
+                                break;
+                            }
+                        }
+                        cnt++;
+                        sum += map[x][y];
+                        if (k == 3 && x == i && y == j) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (flag)
+                    ans = Math.max(ans, sum);
+            }
+        }
+
+        System.out.print(ans);
+    }
+
+    static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+}
